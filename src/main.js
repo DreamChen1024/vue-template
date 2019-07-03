4b958d374756d7bc46b4e11c1fed1705fb6721bd
LAPTOP-7SSMQJ27\86156 <mcchen1024@163.com>
2019-07-03 17:06:31 +0800

    使用scss并初始化样式

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 初始化样式
+import './common/css/reset.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
---
+++ src/views/Home/home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="home">123</div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+    background-color: red;
+}
+</style>

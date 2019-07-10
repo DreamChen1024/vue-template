94483e73bd5751780cb15750b9442ac25c13a6e0
LAPTOP-7SSMQJ27\86156 <mcchen1024@163.com>
2019-07-10 17:49:34 +0800

    proxyTable设置跨域

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://adm-test-zh.ycb51.cn/', //代理的目标地址
+        changeOrigin: true,  //是否开启跨域
+        pathRewrite: {
+          '^/api': '/api' //替代(重定向)
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
